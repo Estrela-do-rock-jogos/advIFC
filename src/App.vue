7658b8c9da447cf27ccad23eecef2107eca68096
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-12-05 15:10:20 -0300

    FEAT: OI

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,103 @@
 <script setup>
-    import { onMounted, ref } from 'vue'
-    import { useWordStore } from '@/stores/word.js'
-    import Loading from 'vue-loading-overlay';
+import { onMounted, ref } from 'vue'
+import { useWordStore } from '@/stores/word.js'
+import Loading from 'vue-loading-overlay'
 
-    const isLoading = ref(false)
+const isLoading = ref(false)
+const wordStore = useWordStore()
 
-    const wordStore = useWordStore();
+onMounted(async () => {
+  isLoading.value = true
+  await wordStore.fetchWord()
+  isLoading.value = false
+})
 
-    onMounted(async() => {
-        isLoading.value = true;
-        await wordStore.fetchWord();
-        isLoading.value = false;
-    });
-    
-
-    
+const um = ref('')
+const dois = ref('')
+const tres = ref('')
+const quatro = ref('')
+const cinco = ref('')
 </script>
 
 <template>
 <section id="main">
-    
+  <h1>AdvIFC pt-pt</h1>
+
+  <form class="quadrado" @submit.prevent>
+    <input type="text" maxlength="1" v-model="um"
+      :class="{
+        green: wordStore.posicao1,
+        yellow: wordStore.existe1
+      }">
+
+    <input type="text" maxlength="1" v-model="dois"
+      :class="{
+        green: wordStore.posicao2,
+        yellow: wordStore.existe2
+      }">
+
+    <input type="text" maxlength="1" v-model="tres"
+      :class="{
+        green: wordStore.posicao3,
+        yellow: wordStore.existe3
+      }">
+
+    <input type="text" maxlength="1" v-model="quatro"
+      :class="{
+        green: wordStore.posicao4,
+        yellow: wordStore.existe4
+      }">
+
+    <input type="text" maxlength="1" v-model="cinco"
+      :class="{
+        green: wordStore.posicao5,
+        yellow: wordStore.existe5
+      }">
+
+    <button
+      v-if="um && dois && tres && quatro && cinco"
+      @click="wordStore.addWord(um, dois, tres, quatro, cinco)"
+    >
+      Enviar
+    </button>
+  </form>
 </section>
 
 <loading v-model:active="isLoading" is-full-page />
-
 </template>
 
-<style scoped></style>
+<style scoped>
+#main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+#main h1 {
+  font-size: 2rem;
+  text-align: center;
+}
+#main .quadrado {
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+#main .quadrado input {
+  width: 55px;
+  height: 55px;
+  font-size: 2rem;
+  text-align: center;
+  border-radius: 8px;
+}
+#main .quadrado input:focus {
+  outline: auto;
+}
+.green {
+  background-color: green;
+  color: white;
+}
+.yellow {
+  background-color: gold;
+  color: black;
+}
+</style>
--- src/stores/word.js
+++ src/stores/word.js
@@ -1,30 +1,68 @@
-import { defineStore } from "pinia";
-import { reactive, computed } from "vue";
-import api from "@/plugins/api";
+import { defineStore } from 'pinia'
+import { reactive, computed, ref } from 'vue'
+import api from '@/plugins/api'
 
 export const useWordStore = defineStore('word', () => {
-    const state = reactive({
-        word: null,
-        letters: null,
-    })
+  const state = reactive({
+    word: null,
+    letters: null,
+    selected: []
+  })
 
-    const word = computed(() => state.word)
-    const letters = computed(() => state.letters)
+  const word = computed(() => state.word)
+  const letters = computed(() => state.letters)
+  const selected = computed(() => state.selected)
 
-    const fetchWord = async () => {
-        const response = await api.get('random')
+  const posicao1 = ref(false)
+  const posicao2 = ref(false)
+  const posicao3 = ref(false)
+  const posicao4 = ref(false)
+  const posicao5 = ref(false)
 
-        if (response.data.word.length !== 5) {
-            return fetchWord()
-        }
+  const existe1 = ref(false)
+  const existe2 = ref(false)
+  const existe3 = ref(false)
+  const existe4 = ref(false)
+  const existe5 = ref(false)
 
-        state.word = response.data.word
-        state.letters = response.data.word.split('')
-    }
+  const fetchWord = async () => {
+    const response = await api.get('random')
+    if (response.data.word.length !== 5) return fetchWord()
+    state.word = response.data.word
+    state.letters = response.data.word.split('')
+  }
 
-    return {
-        word,
-        fetchWord,
-        letters,
-    }
+  function addWord(a, b, c, d, e) {
+    state.selected = [a, b, c, d, e]
+
+    posicao1.value = a === letters.value[0]
+    posicao2.value = b === letters.value[1]
+    posicao3.value = c === letters.value[2]
+    posicao4.value = d === letters.value[3]
+    posicao5.value = e === letters.value[4]
+
+    existe1.value = !posicao1.value && letters.value.includes(a)
+    existe2.value = !posicao2.value && letters.value.includes(b)
+    existe3.value = !posicao3.value && letters.value.includes(c)
+    existe4.value = !posicao4.value && letters.value.includes(d)
+    existe5.value = !posicao5.value && letters.value.includes(e)
+  }
+
+  return {
+    word,
+    letters,
+    selected,
+    fetchWord,
+    addWord,
+    posicao1,
+    posicao2,
+    posicao3,
+    posicao4,
+    posicao5,
+    existe1,
+    existe2,
+    existe3,
+    existe4,
+    existe5
+  }
 })
